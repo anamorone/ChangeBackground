666b9066ee9298ed24a1736c5cfe3b70ad0c5986
Ana <anamoronee30@gmail.com>
2021-12-21 00:12:06 -0300

    1.0

---
+++ script.js
@@ -0,0 +1,38 @@
+let body = document.querySelector("body"),
+    text = document.getElementById("text"),
+    button = document.getElementById("submit"),
+    error = document.querySelector("label"),
+    ValidColor  = /^(#[a-f0-9]{6}|#[a-f0-9]{3}|rgb *\( *[0-9]{1,3}%? *, *[0-9]{1,3}%? *, *[0-9]{1,3}%? *\)|rgba *\( *[0-9]{1,3}%? *, *[0-9]{1,3}%? *, *[0-9]{1,3}%? *, *[0-9]{1,3}%? *\)|black|green|silver|gray|olive|white|yellow|maroon|navy|red|blue|purple|teal|fuchsia|aqua|aliceblue|antiquewhite|aquamarine|azure|beige|bisque|blanchedalmond|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkgrey|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkslategrey|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dimgrey|dodgerblue|firebrick|floralwhite|forestgreen|gainsboro|ghostwhite|gold|goldenrod|greenyellow|grey|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightgrey|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightslategrey|lightsteelblue|lightyellow|limegreen|linen|magenta|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|oldlace|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|skyblue|slateblue|slategray|slategrey|snow|springgreen|steelblue|tan|thistle|tomato|turquoise|violet|wheat|whitesmoke|yellowgreen|)$/i;
+
+
+button.onclick = () => {
+    if(ValidColor.test(text.value.trim())){
+        body.style.backgroundColor = text.value;
+        error.innerHTML = "";
+        text.style.outline = "none";
+        error.style.background = "none";
+        error.style.display = "none";
+      }else {
+        error.innerHTML = "Invalid color";
+        text.style.outline = "4px solid #f82f3d";
+        error.style.display = "initial";
+        error.style.background ="#f82f2f";
+
+      }
+
+      
+}
+
+text.addEventListener("keyup", e => {
+    e.preventDefault();
+    if(e.keyCode === 13){
+        button.click();
+    }
+
+    if(text.value === ""){
+        button.disabled = true;
+    } else {
+        button.disabled = false;
+    }
+
+})
